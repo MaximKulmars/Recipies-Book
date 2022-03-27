78996041e6c472a471b9864569e24e13c94575eb
Maxim Kulmars <klmrswrk@gmail.com>
2022-03-27 17:37:09 +0700

    added while

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def file_reader(): 
     cook_book = {} 
     with open('Recipies.txt', 'r', encoding='utf-8') as file: 
-        for i in range(2): 
+        while True:
             dish_name = file.readline().strip() 
             cook_book[dish_name] = [] 
             qwnty_ingr = int(file.readline()) 
@@ -11,9 +11,14 @@ def file_reader():
                 spl = line.split('|')
                 res = {'ingredient_name': spl[0], 'quantity' : int(spl[1]), 'measure': spl[2]} 
                 r.append(res)
-            cook_book[dish_name] = r 
-    return(cook_book) 
- 
+            file.readline()
+            cook_book[dish_name] = r
+            
+            break
+        
+             
+        return(cook_book) 
+        
 print(file_reader())
 
 #def ingr_format():
